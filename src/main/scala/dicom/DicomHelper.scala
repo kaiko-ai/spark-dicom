ee3320a9f21d3adf21f657e9cb9a37aa6d867542
Guillaume Desforges <guillaume.desforges.pro@gmail.com>
2021-11-15 18:47:04 +0100

    Demonstrate reading DICOM tags

--- src/main/scala/dicom/DicomHelper.scala
+++ src/main/scala/dicom/DicomHelper.scala
@@ -1,20 +1,79 @@
 package ai.kaiko.dicom
 
+import org.dcm4che3.data.Keyword
 import org.dcm4che3.imageio.plugins.dcm.DicomImageReaderSpi
 import org.dcm4che3.io.DicomInputStream
+import org.dcm4che3.util.TagUtils
 
 import java.awt.image.BufferedImage
 import java.io.File
+import org.dcm4che3.data.VR
 
 object DicomHelper {
-  def readDicomImage(file: File): BufferedImage = {
+  def readDicomTagsAsMap(
+      file: File
+  ): Map[String, Option[String]] = {
+    val dicomIn = new DicomInputStream(file)
+    val attrs = dicomIn.readDataset
+    attrs.tags
+      .map(tag =>
+        (
+          Keyword.valueOf(tag),
+          attrs.getVR(tag) match {
+            case VR.AE => None
+            case VR.AS => None
+            case VR.AT => None
+            case VR.CS => None
+            case VR.DA => None
+            case VR.DS => None
+            case VR.DT => None
+            case VR.FD => None
+            case VR.FL => None
+            case VR.IS => None
+            case VR.LO => None
+            case VR.LT => None
+            case VR.OB => None
+            case VR.OD => None
+            case VR.OF => None
+            case VR.OL => None
+            case VR.OV => None
+            case VR.OW => None
+            // Person Name
+            case VR.PN => Some(attrs.getString(tag))
+            case VR.SH => None
+            case VR.SL => None
+            case VR.SQ => None
+            case VR.SS => None
+            case VR.ST => None
+            case VR.SV => None
+            case VR.TM => None
+            case VR.UC => None
+            case VR.UI => None
+            case VR.UL => None
+            case VR.UN => None
+            case VR.UR => None
+            case VR.US => None
+            // Unlimited Text
+            case VR.UT => Some(attrs.getString(tag))
+            case VR.UV => None
+          }
+        )
+      )
+      .toMap
+  }
+
+  def readDicomImage(file: File): Option[BufferedImage] = {
     val imgReaderSpi = new DicomImageReaderSpi
     val imgReader = imgReaderSpi.createReaderInstance(null)
 
     val dicomIn = new DicomInputStream(file)
     imgReader.setInput(dicomIn)
     val numImg = imgReader.getNumImages(true)
-    val imgReadParam = imgReader.getDefaultReadParam
-    imgReader.read(0, null)
+    if (numImg > 0) {
+      val imgReadParam = imgReader.getDefaultReadParam
+      Some(imgReader.read(0, null))
+    } else {
+      None
+    }
   }
 }
--- src/test/scala/dicom/TestDicomHelper.scala
+++ src/test/scala/dicom/TestDicomHelper.scala
@@ -2,6 +2,8 @@ package ai.kaiko.dicom
 
 import org.apache.log4j.Level
 import org.apache.log4j.LogManager
+import org.dcm4che3.data
+import org.dcm4che3.util.TagUtils
 import org.scalatest._
 import org.scalatest.flatspec.AnyFlatSpec
 
@@ -17,10 +19,25 @@ class TestDicomFile extends AnyFlatSpec {
   val SOME_DICOM_FILEPATH =
     "src/test/resources/Pancreatic-CT-CBCT-SEG/Pancreas-CT-CB_001/07-06-2012-NA-PANCREAS-59677/201.000000-PANCREAS DI iDose 3-97846/1-001.dcm"
 
-  "Dicom" should "be read" in {
+  "Dicom tags" should "be read" in {
+    val file = new File(SOME_DICOM_FILEPATH)
+    assert(file.exists)
+    val metadata = DicomHelper.readDicomTagsAsMap(file)
+    assert(metadata.contains("PatientName"))
+    assert(
+      metadata
+        .get(
+          data.Keyword.valueOf(data.Tag.PatientName)
+        )
+        .get
+        .get === "Pancreas-CT-CB_001"
+    )
+  }
+
+  "Dicom image" should "be read" in {
     val file = new File(SOME_DICOM_FILEPATH)
     assert(file.exists)
     val img = DicomHelper.readDicomImage(file)
-    assert(img !== null)
+    assert(img.isDefined)
   }
 }
