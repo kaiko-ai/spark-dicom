d109553ce99ccbc3dd6e715da67a45a5d4484991
Clément Hurlin <smelc@users.noreply.github.com>
2022-04-11 17:54:17 +0200

    Shellcheck *.sh files in the CI (#50)

--- scripts/check_license_headers.sh
+++ scripts/check_license_headers.sh
@@ -1,11 +1,13 @@
+#!/usr/bin/env bash
+
 set -e
 echo "Checking that a Apache 2 license header is present at the top of all scala files in src/"
 
-src_files_with_missing_license=$(find src -type f -name '*.scala' -exec bash -c '[ ! -z "$(head -1 {} | grep -v "// Licensed to the Apache Software Foundation (ASF) under one")" ] && echo "{}"' \;)
+src_files_with_missing_license=$(find src -type f -name '*.scala' -exec bash -c '[ ! -z "$(head -1 "$1" | grep -v "// Licensed to the Apache Software Foundation (ASF) under one")" ] && echo "$1"' shell {} \;)
 if [ "$src_files_with_missing_license" ]
 then
     echo "Found unlicensed files!"
-    echo $src_files_with_missing_license
+    echo "$src_files_with_missing_license"
     exit 1
 fi
 echo "All ok"
